5086bc345e207e724f34700a8561106deced1a5c
Charlitchi <Charlitchi@users.noreply.github.com>
2019-03-05 18:47:33 +0100

    Did the tutorial from thepolyglotdeveloper.com

---
+++ tutorial/tosend.js
@@ -0,0 +1,3 @@
+curl -X POST -H 'content-type:application/json' -d '{"firstname":"Maria","lastname":"Raboy"}' http://localhost:3000/person
+curl -X GET http://localhost:3000/people
+curl -X GET http://localhost:3000/person/5c7eaa7e1f92438ea27c2512
